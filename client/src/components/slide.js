dea271bef08fd6371ddfe152f4cdbab0ef97a60b
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-03-28 15:15:19 +0300

    Added preview page for style testing

--- client/src/components/slide.js
+++ client/src/components/slide.js
@@ -1,8 +1,22 @@
 import React from "react"
+import slideStyles from "./slide.module.css"
 
 // A heading with the year for a paticular slide
 // A flexbox with two parts:
 // 1. A heading and a paragraph
 // 2. Image carousel
 
-const Slide = ({ slideYear }) => <header>{slideYear}</header>
+const Slide = ({ props }) => (
+  <div className={slideStyles.year}>
+    {props.year}
+    <div className={slideStyles.card}>
+      <div className={slideStlyes.description}>
+        <h1>{props.title}</h1>
+        <p>{props.text}</p>
+        <img src={props.image} className={slideStyles.carousel} alt="" />
+      </div>
+    </div>
+  </div>
+)
+
+export default Slide
---
+++ client/src/pages/preview-slide.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Slide from "../components/slide"
+
+export default () => (
+  <Slide
+    year="1978"
+    title="Space Invaders"
+    text="That is a game and this is some text about it. It was supposedly a significant achievemnt in the world of pixel art. More info coming soon"
+  />
+)
