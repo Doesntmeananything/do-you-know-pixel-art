92314f0a6b843ff760df59c3bd1057ed614e7617
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-03-28 12:47:02 +0300

    Starting on slide component

---
+++ client/src/components/slide.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+// A heading with the year for a paticular slide
+// A flexbox with two parts:
+// 1. A heading and a paragraph
+// 2. Image carousel
+
+const Slide = ({ slideYear }) => <header>{slideYear}</header>
