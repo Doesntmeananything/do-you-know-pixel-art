7a7a396001077b64d681730a2e72d2bdf8221188
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-03-29 19:36:44 +0300

    Added components

---
+++ client/src/components/card.js
@@ -0,0 +1,14 @@
+import React from "react"
+import cardStyles from "./card.module.css"
+
+const Card = props => (
+  <div className={cardStyles.container}>
+    <div className={cardStyles.description}>
+      <h1>{props.title}</h1>
+      <p>{props.text}</p>
+    </div>
+    <div className={cardStyles.media}>{props.media}</div>
+  </div>
+)
+
+export default Card
--- client/src/pages/slide.js
+++ client/src/pages/slide.js
@@ -1,10 +1,16 @@
 import React from "react"
 import TitularYear from "../components/titular-year"
 import BackgroundLayer from "../components/background-layer"
+import Card from "../components/card"
 
 export default () => (
   <>
     <BackgroundLayer />
     <TitularYear year="1975" />
+    <Card
+      title="Space Invaders"
+      text="This is a text about this game."
+      media="Some image"
+    />
   </>
 )
