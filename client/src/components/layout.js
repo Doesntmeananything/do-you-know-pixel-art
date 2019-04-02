0b2d925093a62eafee34a0a548d7ca27347c1860
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-02 14:02:19 +0300

    Added Grid List Component

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -3,15 +3,18 @@ import { withStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import Typography from "@material-ui/core/Typography"
+import ImageGridList from "./imageGridList"
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
+    minHeight: "90vh",
+    justifyContent: "center",
+    alignItems: "center",
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    margin: "auto",
-    maxWidth: 800,
+    minWidth: 800,
     minHeight: 400,
   },
 })
@@ -19,19 +22,22 @@ const styles = theme => ({
 function Layout(props) {
   const { classes } = props
   return (
-    <div className={classes.root}>
+    <Grid container className={classes.root}>
       <Paper className={classes.paper}>
-        <Grid container>
+        <Grid container spacing={16}>
           <Grid item>
-            <Typography variant="h1">Space Invaders</Typography>
+            <Typography variant="h3">Space Invaders</Typography>
             <Typography variant="body1">
               This is some text about the game and its visual style. More stuff
               coming soon.
             </Typography>
           </Grid>
+          <Grid item>
+            <ImageGridList />
+          </Grid>
         </Grid>
       </Paper>
-    </div>
+    </Grid>
   )
 }
 
---
+++ client/src/components/tileData.js
@@ -0,0 +1,51 @@
+import image from "../../public/static/space-invaders.png"
+
+const tileData = [
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+  {
+    img: image,
+    cols: 1,
+    title: "Space Invaders",
+  },
+]
+
+export default tileData
