a42c778c4ee8ae4ef1bce86ce355eb2ede37d99e
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-02 11:44:26 +0300

    Removed default layout

--- client/src/pages/slide.js
+++ client/src/pages/slide.js
@@ -1,7 +1,6 @@
 import React from "react"
 import TitularYear from "../components/titular-year"
 import BackgroundLayer from "../components/background-layer"
-import Card from "../components/card"
 import PaperSheet from "../components/paper-sheet"
 import Button from "../components/button"
 
