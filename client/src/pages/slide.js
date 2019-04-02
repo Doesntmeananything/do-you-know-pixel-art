aa45aed07eb34fc78c1de3484e2ee892c97bc2e3
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-02 11:35:30 +0300

    Removed Card component

--- client/src/pages/slide.js
+++ client/src/pages/slide.js
@@ -3,10 +3,12 @@ import TitularYear from "../components/titular-year"
 import BackgroundLayer from "../components/background-layer"
 import Card from "../components/card"
 import PaperSheet from "../components/paper-sheet"
+import Button from "../components/button"
 
 export default () => (
   <>
     <TitularYear year="1975" />
     <PaperSheet />
+    <Button />
   </>
 )
