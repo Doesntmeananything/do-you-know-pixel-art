bf29cfd8dfc4c87eb0d0b4f2549ac415ed94fb8b
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-05 12:12:30 +0300

    Centered modal

--- client/src/components/albumModal.js
+++ client/src/components/albumModal.js
@@ -1,30 +1,19 @@
 import React from "react"
 import { withStyles } from "@material-ui/core/styles"
 import Modal from "@material-ui/core/Modal"
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10
-}
-
-function getModalStyle() {
-  const top = 50 + rand()
-  const left = 50 + rand()
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  }
-}
+import Grid from "@material-ui/core/Grid"
 
 const styles = theme => ({
   paper: {
-    position: "absolute",
-    width: theme.spacing.unit * 50,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 4,
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
     outline: "none",
+  },
+  modal: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 })
 
@@ -32,18 +21,19 @@ class AlbumModal extends React.Component {
   render() {
     const { classes } = this.props
     return (
-      <div>
-        <Modal
-          aria-labelledby="game-gallery"
-          aria-describedby="check-art-in-detail"
-          open={this.props.open}
-          onClose={this.props.handleClose}
-        >
-          <div style={getModalStyle()} className={classes.paper}>
-            {this.props.children}
-          </div>
-        </Modal>
-      </div>
+      <Grid container className={classes.root}>
+        <Grid item>
+          <Modal
+            aria-labelledby="game-gallery"
+            aria-describedby="check-art-in-detail"
+            open={this.props.open}
+            onClose={this.props.handleClose}
+            className={classes.modal}
+          >
+            <div className={classes.paper}>{this.props.children}</div>
+          </Modal>
+        </Grid>
+      </Grid>
     )
   }
 }
