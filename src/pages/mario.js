d38eb584ec993a23e2f0b797e3ff94750a403a14
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-01 13:23:36 +0300

    Remove unused import

--- src/pages/mario.js
+++ src/pages/mario.js
@@ -2,7 +2,6 @@ import React from "react"
 import Mario from "../games/Mario"
 import "typeface-roboto"
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
-import blue from "@material-ui/core/colors/blue"
 
 const theme = createMuiTheme({
   palette: {
