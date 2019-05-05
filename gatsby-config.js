aede718a05ac40072227918c760acbf22c7b5dcf
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-05 16:44:06 +0300

    Add transition library

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-transition-link`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
