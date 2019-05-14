b125ce4a12f280804cd63444a5bc5877df2ed521
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-14 14:44:59 +0300

    Add arrow animation

--- src/hero/heroBackground.js
+++ src/hero/heroBackground.js
@@ -1,162 +1,82 @@
 import React from "react"
-import PixelIcon from "@material-ui/icons/Stop"
+import ArrowDownward from "@material-ui/icons/ArrowDownward"
 import { withStyles } from "@material-ui/core/styles"
 import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons"
 import HeroTitleText from "./heroTitleText"
-
-const colors = [
-  "#e65100",
-  "#212121",
-  "#5c6bc0",
-  "#1a237e",
-  "#fbc02d",
-  "#b71c1c",
-  "#42a5f5",
-  "#424242",
-  "#330e62",
-]
+import HeroSecondaryText from "./heroSecondaryText"
+import { FirstLayer, SecondLayer, ThirdLayer } from "./pixelAnimation/"
 
 const styles = theme => ({
   root: {
     background: "#f06292",
     width: "100vw",
     height: "100vh",
-    overflow: "hidden",
   },
-  icon: {
-    animation: "rise var(--speed) infinite linear",
-    color: "var(--icon-color)",
+  downIcon: {
+    cursor: "pointer",
+    animation: "delayedFade 1s ease-out 5s infinite alternate",
+    position: "absolute",
+    bottom: "5px",
+    left: "40%",
+    opacity: 0,
+    fontSize: 60,
   },
-  "@keyframes rise": {
-    from: { transform: `translate(var(--x), 110vh)` },
-    to: { transform: `translate(var(--x), -10vh)` },
+  "@keyframes delayedFade": {
+    from: { transform: "translate(0, 10vh)" },
+    to: { opacity: 1, transform: "translate(0, 0)" },
   },
 })
 
-function LandingBackground(props) {
-  const { classes } = props
-  return (
-    <div className={classes.root}>
-      <Parallax pages={2}>
-        {/* First layer */}
-        <ParallaxLayer offset={0} speed={0.5}>
-          <PixelIcon
-            className={classes.icon}
-            fontSize="small"
-            style={{
-              "--icon-color": colors[0],
-              "--x": "12vw",
-              "--speed": "5.5s",
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            fontSize="small"
-            style={{
-              "--icon-color": colors[2],
-              "--x": "34vw",
-              "--speed": "6s",
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            fontSize="small"
-            style={{
-              "--icon-color": colors[4],
-              "--x": "64vw",
-              "--speed": "7s",
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            fontSize="small"
-            style={{
-              "--icon-color": colors[6],
-              "--x": "87vw",
-              "--speed": "6.5s",
-            }}
-          />
-        </ParallaxLayer>
-        {/* Second layer */}
-        <ParallaxLayer offset={0} speed={1.5}>
-          <PixelIcon
-            className={classes.icon}
-            fontSize="large"
-            style={{ "--icon-color": colors[1], "--x": "5vw", "--speed": "4s" }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            fontSize="large"
-            style={{
-              "--icon-color": colors[3],
-              "--x": "23vw",
-              "--speed": "3.2s",
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            fontSize="large"
-            style={{
-              "--icon-color": colors[5],
-              "--x": "55vw",
-              "--speed": "3.5s",
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            fontSize="large"
-            style={{
-              "--icon-color": colors[7],
-              "--x": "76vw",
-              "--speed": "3s",
-            }}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={1.5}>
-          <HeroTitleText />
-        </ParallaxLayer>
-        {/* Third Layer */}
-        <ParallaxLayer offset={0} speed={2.5}>
-          <PixelIcon
-            className={classes.icon}
-            style={{
-              "--icon-color": colors[3],
-              "--x": "5vw",
-              "--speed": "2.5s",
-              fontSize: 60,
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            style={{
-              "--icon-color": colors[2],
-              "--x": "24vw",
-              "--speed": "2.2s",
-              fontSize: 60,
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            style={{
-              "--icon-color": colors[0],
-              "--x": "45vw",
-              "--speed": "2s",
-              fontSize: 60,
-            }}
-          />
-          <PixelIcon
-            className={classes.icon}
-            style={{
-              "--icon-color": colors[9],
-              "--x": "56vw",
-              "--speed": "1.8s",
-              fontSize: 60,
-            }}
-          />
-        </ParallaxLayer>
-      </Parallax>
-    </div>
-  )
+class LandingBackground extends React.Component {
+  render() {
+    const { classes } = this.props
+
+    return (
+      <div className={classes.root}>
+        <Parallax
+          ref={ref => (this.parallax = ref)}
+          pages={2}
+          scrolling={false}
+        >
+          {/* Page 1 */}
+          {/* First layer animation */}
+          <ParallaxLayer offset={0} speed={0.5}>
+            <FirstLayer />
+          </ParallaxLayer>
+          {/* Second layer animation */}
+          <ParallaxLayer offset={0} speed={1.5}>
+            <SecondLayer />
+          </ParallaxLayer>
+          {/* Title */}
+          <ParallaxLayer offset={0} speed={1.5}>
+            <HeroTitleText />
+          </ParallaxLayer>
+          {/* Third Layer animation */}
+          <ParallaxLayer
+            offset={0}
+            speed={2.5}
+            onClick={() => this.parallax.scrollTo(1)}
+          >
+            <ThirdLayer />
+            <ArrowDownward className={classes.downIcon} />
+          </ParallaxLayer>
+          {/* Page 2 */}
+          {/* All animation layers */}
+          <ParallaxLayer offset={1} speed={1.5}>
+            <FirstLayer />
+            <SecondLayer />
+            <ThirdLayer />
+          </ParallaxLayer>
+          {/* Text */}
+          <ParallaxLayer offset={1} speed={0.5}>
+            <HeroSecondaryText
+              handleParallaxBack={() => this.parallax.scrollTo(0)}
+            />
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+    )
+  }
 }
 
 export default withStyles(styles)(LandingBackground)
