dec3e2fb4146c9dcc640ff93520d5df465f2a4f9
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-14 19:25:33 +0300

    Fix title

--- src/games/Castlevania3/index.js
+++ src/games/Castlevania3/index.js
@@ -13,8 +13,8 @@ function Slide(props) {
     <SlidePaper color={color}>
       <GameText
         year="1989"
-        title="Castlevania 3"
-        description="The endless possibilities of pixel art opened up a lot of stylistic niches, and it wasn't long before darker and more grounded aesthetic became prominent. By borrowing from the gothic horror ethos, Castlevania espoused medieval imagery with pop-culture sensibilities. Even though it was bound my many technical limitations of its time, this mix proved to be as volatile as it was immensely appealing. In many ways, Castlevania 3 became the apogee of the style that the series itself created in the middle 80s."
+        title="Castlevania III: Dracula's Curse"
+        description="The endless possibilities of pixel art opened up a lot of stylistic niches, and it wasn't long before darker and more grounded aesthetic became prominent. By borrowing from the gothic horror ethos, Castlevania espoused medieval imagery with pop-culture sensibilities. Even though it was bound my many technical limitations of its time, this mix proved to be as volatile as it was immensely appealing. In many ways, Castlevania III became the apogee of the style that the series itself created in the middle 80s."
       />
       <ImageGridList
         tileData={tileData}
