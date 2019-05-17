d7b2dd6bdf442a469c33262d5d52ca1033b1553e
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-17 20:14:24 +0300

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Do You Know Pixel Art?`,
+        short_name: `Pixel Art`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f06292`,
+        theme_color: `#f06292`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/pixel-icon.png`, // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
