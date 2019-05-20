fd54ff57eb9c2a8a796ed4b60a8a36df0cdb52cd
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-20 18:56:24 +0300

    Add typography plugin to make Roboto default

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,12 @@ module.exports = {
         crossOrigin: `use-credentials`,
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  headerFontFamily: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+  bodyFontFamily: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+})
+
+export default typography
